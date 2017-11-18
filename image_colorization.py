35d5271cf9894c1f53367a95cf129cd59c0fd6dc
Wilson Yan <wilson1.yan@berkeley.edu>
2017-11-17 20:56:06 -0800

    refactoring and reorganizing code

---
+++ image_colorization.py
@@ -0,0 +1,86 @@
+from PIL import Image
+from skimage import color
+from scipy.ndimage import zoom
+
+import torch
+import torch.utils.data as data
+import torch.nn as nn
+
+import numpy as np
+
+
+class ColorizationDataset(data.Dataset):
+    def __init__(self, img_names, preprocessor, image_size_in=256, image_size_out=64):
+        super(ColorizationDataset, self).__init__()
+        self.image_size_in = image_size_in
+        self.image_size_out = image_size_out
+        self.img_names = img_names
+        self.preprocessor = preprocessor
+    
+    def __getitem__(self, index):
+        image = Image.open(self.img_names[index])
+        image = image.resize((self.image_size_in, self.image_size_in))
+        image_rgb = np.array(image)
+        image_lab = color.rgb2lab(image_rgb)
+        image_lab = image_lab.transpose(2, 0, 1)
+        
+        input, label = image_lab[0, :, :] - 50, image_lab[1:, :, :]
+        label = zoom(label, (1, self.image_size_out / self.image_size_in, self.image_size_out / self.image_size_in))
+        
+        label = self.preprocessor(label)
+        return torch.FloatTensor(input).unsqueeze(0), torch.FloatTensor(label)
+    
+    def __len__(self):
+        return len(self.img_names)
+
+class ImageColorizer(nn.Module):
+    def __init__(self, n_outputs, temperature=0.38):
+        super(ImageColorizer, self).__init__()
+        self.n_classes = n_classes
+        
+        self.moduleList = nn.ModuleList()
+        self.moduleList.append(self.conv_block(1, 64, 2, downsample=True))
+        self.moduleList.append(self.conv_block(64, 128, 2, downsample=True))
+        self.moduleList.append(self.conv_block(128, 256, 3, downsample=True))
+        self.moduleList.append(self.conv_block(256, 512, 3))
+        self.moduleList.append(self.conv_block(512, 512, 3, dilation=2, padding=2))
+        self.moduleList.append(self.conv_block(512, 512, 3, dilation=2, padding=2))
+        self.moduleList.append(self.conv_block(512, 512, 3))
+        self.moduleList.append(self.deconv_block())
+                         
+        self.conv_class = nn.Conv2d(256, n_outputs, 1)
+        self.temperature = temperature
+        
+    def conv_block(self, in_dim, out_dim, n_convs, dilation=1, padding=1, downsample=False):
+        block = []
+        for i in range(0, n_convs):
+            if i == 0:
+                in_feat, stride = in_dim, 1
+            elif i == n_convs - 1:
+                in_feat, stride = out_dim, 2 if downsample else 1
+            else:
+                in_feat, stride = out_dim, 1
+                
+            block.append(nn.Conv2d(in_feat, out_dim, 3, padding=padding, stride=stride, dilation=dilation))
+            block.append(nn.ReLU(inplace=True))
+
+        block.append(nn.BatchNorm2d(out_dim))
+        return nn.Sequential(*block)
+    
+    def deconv_block(self):
+        block = []
+        block.append(nn.ConvTranspose2d(512, 256, 4, padding=1, stride=2))
+        block.append(nn.ReLU(inplace=True))
+        block.append(nn.Conv2d(256, 256, 3, padding=1))
+        block.append(nn.ReLU(inplace=True))
+        block.append(nn.Conv2d(256, 256, 3, padding=1))
+        block.append(nn.ReLU(inplace=True))
+        return nn.Sequential(*block)
+    
+    def forward(self, input):
+        for m in self.moduleList:
+            input = m(input)
+
+        input = self.conv_class(input)
+        input *= 1 / self.temperature        
+        return input
---
+++ utils.py
@@ -0,0 +1,53 @@
+import glob
+
+import numpy as np
+
+GRAY_IMAGES = ['data/ILSVRC2014_DET_train/ILSVRC2014_train_0000/ILSVRC2014_train_00008823.JPEG',
+          'data/ILSVRC2014_DET_train/ILSVRC2014_train_0000/ILSVRC2014_train_00000278.JPEG',
+          'data/ILSVRC2014_DET_train/ILSVRC2014_train_0000/ILSVRC2014_train_00008774.JPEG',
+          'data/ILSVRC2014_DET_train/ILSVRC2014_train_0000/ILSVRC2014_train_00002194.JPEG',
+          'data/ILSVRC2014_DET_train/ILSVRC2014_train_0000/ILSVRC2014_train_00004122.JPEG',
+          'data/ILSVRC2014_DET_train/ILSVRC2014_train_0000/ILSVRC2014_train_00001535.JPEG']
+
+
+def get_images(n=None, random=False):
+    image_list = glob.glob('data/ILSVRC2014_DET_train/ILSVRC2014_train_0000/*')
+    image_list = [img for img in image_list if img not in GRAY_IMAGES]
+    if n is not None:
+        image_list = image_list[:n]
+    
+    return image_list
+
+
+def gaussian_smooth(seq, sigma=5):
+    x_vals = np.arange(len(seq))
+    smoothed_seq = np.zeros(len(seq))
+    for i in range(len(seq)):
+        kernel = np.exp(-(x_vals - i) ** 2 / (2 * sigma ** 2))
+        kernel /= np.sum(kernel)
+        smoothed_seq[i] = np.sum(kernel * seq)
+    return smoothed_seq
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.xavier_normal(m.weight.data)
+    elif classname.find('Linear') != -1:
+        nn.init.xavier_normal(m.weight.data)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+def get_smoothed_label(label, nearest_neighbors, ab2cat, sigma=5, quantize_step=10):
+    n_spaces, (k, m, n) = len(ab2cat), label.shape
+    label = np.clip(label, -128, 128)
+    quantized_label = np.zeros((n_spaces, m, n))
+    for i in range(m):
+        for j in range(n):
+            nearest_5 = nearest_neighbors[(int(label[0, i, j]), int(label[1, i, j]))]
+            quantized = np.zeros(n_spaces)
+            for ab in nearest_5:
+                quantized[ab] = 1 / (np.linalg.norm(ab - label[:, i, j]) + 1)
+            quantized /= np.sum(quantized)
+            quantized_label[:, i, j] = quantized
+    return quantized_label
